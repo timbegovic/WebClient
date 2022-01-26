004fc641efa5b57f87de5bf46511669a614fbfd1
TIM BEGOVIC <tim.begovic03@gmail.com>
2022-01-26 21:01:32 +0100

    changed files

--- webclient.cpp
+++ webclient.cpp
@@ -1,7 +1,48 @@
 #include "webclient.h"
+#include <thread>
 
 Webclient::Webclient(QWidget *parent) :
     QWidget(parent)
 {
     setupUi(this);
+
+
+    connect(hostname, &QLineEdit::returnPressed, this, &Webclient::gobutton_clicked);
+
+    m_socket = new QTcpSocket(this);
+    connect(m_socket, &QTcpSocket::connected, this, &Webclient::connectsocket);
+    connect(m_socket, &QTcpSocket::readyRead, this, &Webclient::disconnect);
+    connect(gobutton, &QPushButton::clicked, this, &Webclient::gobutton_clicked);
+
+
+}
+
+void Webclient::outputMessage(QString message)
+{
+    outputmessage->clear();
+    outputmessage->setText(message);
+}
+
+void Webclient::connectsocket()
+{
+    m_socket->write(QString("GET / HTTP/1.1\r\nHost:" + m_search + "\r\n\r\n").toLocal8Bit());
+}
+
+void Webclient::disconnect()
+{
+    outputMessage(m_socket->readAll());
+    m_socket->disconnectFromHost();
+}
+
+void Webclient::gobutton_clicked()
+{
+
+    if(hostname->text().isEmpty())
+    {
+        outputMessage("Couldn't find adress.");
+    }
+    else
+    {
+        m_socket->connectToHost(hostname->text(), m_port);
+    }
 }
--- webclient.h
+++ webclient.h
@@ -2,6 +2,9 @@
 #define WEBCLIENT_H
 
 #include "ui_webclient.h"
+#include <QDebug>
+#include <QAbstractSocket>
+#include <QTcpSocket>
 
 class Webclient : public QWidget, private Ui::Webclient
 {
@@ -9,6 +12,19 @@ class Webclient : public QWidget, private Ui::Webclient
 
 public:
     explicit Webclient(QWidget *parent = nullptr);
+
+private slots:
+    void gobutton_clicked();
+
+private:
+    void connectsocket();
+    void outputMessage(QString message);
+    void disconnect();
+
+private:
+    QTcpSocket *m_socket = nullptr;
+    QString m_search;
+    const char m_port = 80;
 };
 
 #endif // WEBCLIENT_H
